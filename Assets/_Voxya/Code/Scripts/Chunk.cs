630c7feac887d836615dfe85d57cda6d2f62c9de
Viesmu <v.esnaola.m@gmail.com>
2025-10-17 00:36:31 +0200

    por fin funciona la creacion de mundo basica procedural

--- Assets/_Voxya/Code/Scripts/Chunk.cs
+++ Assets/_Voxya/Code/Scripts/Chunk.cs
@@ -9,11 +9,11 @@ public class Chunk : MonoBehaviour
     public static int chunkHeight = 100;
     private BlockType[,,] voxelMap = new BlockType[chunkSize, chunkHeight, chunkSize];
 
+    private WorldGenerator world;
     private List<Vector3> vertices = new List<Vector3>();
     private List<int> triangles = new List<int>();
     private List<Vector2> uvs = new List<Vector2>();
 
-    // --- Definición del Atlas de Texturas ---
     private const float TextureAtlasSizeInBlocks = 2f;
     private const float NormalizedBlockTextureSize = 1f / TextureAtlasSizeInBlocks;
 
@@ -22,67 +22,76 @@ public class Chunk : MonoBehaviour
     private static readonly Vector2 DirtTexture = new Vector2(0, 0);
     private static readonly Vector2 StoneTexture = new Vector2(1, 0);
 
-    // --- TABLAS DE DATOS DE VOXEL ---
-    private static readonly Vector3[] VoxelVertices = new Vector3[8]
+    public void Initialize(WorldGenerator world)
     {
-        new Vector3(0.0f, 0.0f, 0.0f), // 0
-        new Vector3(1.0f, 0.0f, 0.0f), // 1
-        new Vector3(1.0f, 1.0f, 0.0f), // 2
-        new Vector3(0.0f, 1.0f, 0.0f), // 3
-        new Vector3(0.0f, 0.0f, 1.0f), // 4
-        new Vector3(1.0f, 0.0f, 1.0f), // 5
-        new Vector3(1.0f, 1.0f, 1.0f), // 6
-        new Vector3(0.0f, 1.0f, 1.0f)  // 7
-    };
-
-    private static readonly int[,] VoxelFaces = new int[6, 4]
-    {
-        {0, 3, 2, 1}, // Cara Trasera
-        {5, 6, 7, 4}, // Cara Frontal
-        {3, 7, 6, 2}, // Cara Superior
-        {1, 5, 4, 0}, // Cara Inferior
-        {4, 7, 3, 0}, // Cara Izquierda
-        {2, 6, 5, 1}  // Cara Derecha
-    };
-
-    private static readonly Vector2[] VoxelUvs = new Vector2[4]
-    {
-        new Vector2(0.0f, 0.0f),
-        new Vector2(0.0f, 1.0f),
-        new Vector2(1.0f, 0.0f),
-        new Vector2(1.0f, 1.0f)
-    };
-
-    void Start()
-    {
+        this.world = world;
         PopulateVoxelMap();
         GenerateChunkMesh();
         CreateMesh();
     }
 
-    void AddVoxelFace(Vector3 position, int faceIndex, BlockType blockType)
+    void GenerateChunkMesh()
     {
-        Vector2 textureCoord = GetTextureCoord(blockType, faceIndex);
-        int vertexIndex = vertices.Count;
+        for (int y = 0; y < chunkHeight; y++)
+            for (int x = 0; x < chunkSize; x++)
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    BlockType blockType = voxelMap[x, y, z];
+                    if (blockType == BlockType.Air)
+                        continue;
+
+                    Vector3 blockPos = new Vector3(x, y, z);
+                    for (int i = 0; i < 6; i++)
+                    {
+                        if (IsFaceVisible(blockPos, i))
+                        {
+                            CreateFace(blockType, blockPos, i);
+                        }
+                    }
+                }
+    }
+
+    bool IsFaceVisible(Vector3 blockPos, int faceIndex)
+    {
+        Vector3 neighborPos = blockPos + FaceNormals[faceIndex];
+
+        if (neighborPos.x < 0 || neighborPos.x >= chunkSize ||
+            neighborPos.y < 0 || neighborPos.y >= chunkHeight ||
+            neighborPos.z < 0 || neighborPos.z >= chunkSize)
+        {
+            return world.GetBlockAt(transform.position + neighborPos) == BlockType.Air;
+        }
+        else
+        {
+            return voxelMap[(int)neighborPos.x, (int)neighborPos.y, (int)neighborPos.z] == BlockType.Air;
+        }
+    }
+
+    void CreateFace(BlockType blockType, Vector3 blockPos, int faceIndex)
+    {
+        int vertCount = vertices.Count;
 
         for (int i = 0; i < 4; i++)
         {
-            vertices.Add(position + VoxelVertices[VoxelFaces[faceIndex, i]]);
+            vertices.Add(blockPos + VoxelFaceData[faceIndex, i]);
         }
 
-        uvs.Add(new Vector2(textureCoord.x * NormalizedBlockTextureSize, textureCoord.y * NormalizedBlockTextureSize) + VoxelUvs[0] * NormalizedBlockTextureSize);
-        uvs.Add(new Vector2(textureCoord.x * NormalizedBlockTextureSize, textureCoord.y * NormalizedBlockTextureSize) + VoxelUvs[1] * NormalizedBlockTextureSize);
-        uvs.Add(new Vector2(textureCoord.x * NormalizedBlockTextureSize, textureCoord.y * NormalizedBlockTextureSize) + VoxelUvs[2] * NormalizedBlockTextureSize);
-        uvs.Add(new Vector2(textureCoord.x * NormalizedBlockTextureSize, textureCoord.y * NormalizedBlockTextureSize) + VoxelUvs[3] * NormalizedBlockTextureSize);
+        // Este orden de triangulación es correcto y estándar
+        triangles.Add(vertCount);
+        triangles.Add(vertCount + 1);
+        triangles.Add(vertCount + 2);
+        triangles.Add(vertCount);
+        triangles.Add(vertCount + 2);
+        triangles.Add(vertCount + 3);
 
-        // --- ¡LA CORRECCIÓN DEFINITIVA ESTÁ AQUÍ! ---
-        // Este es el orden de bobinado (winding order) correcto para los vértices.
-        triangles.Add(vertexIndex);
-        triangles.Add(vertexIndex + 1);
-        triangles.Add(vertexIndex + 2);
-        triangles.Add(vertexIndex + 2);
-        triangles.Add(vertexIndex + 1);
-        triangles.Add(vertexIndex + 3);
+        Vector2 textureCoord = GetTextureCoord(blockType, faceIndex);
+        float x = textureCoord.x * NormalizedBlockTextureSize;
+        float y = textureCoord.y * NormalizedBlockTextureSize;
+
+        uvs.Add(new Vector2(x, y));
+        uvs.Add(new Vector2(x, y + NormalizedBlockTextureSize));
+        uvs.Add(new Vector2(x + NormalizedBlockTextureSize, y + NormalizedBlockTextureSize));
+        uvs.Add(new Vector2(x + NormalizedBlockTextureSize, y));
     }
 
     Vector2 GetTextureCoord(BlockType blockType, int faceIndex)
@@ -91,29 +100,11 @@ public class Chunk : MonoBehaviour
         {
             return (faceIndex == 2) ? GrassTopTexture : GrassSideTexture;
         }
-        else if (blockType == BlockType.Dirt)
+        if (blockType == BlockType.Dirt)
         {
             return DirtTexture;
         }
-        else // Stone
-        {
-            return StoneTexture;
-        }
-    }
-
-    void CreateVoxel(Vector3 position)
-    {
-        int x = (int)position.x;
-        int y = (int)position.y;
-        int z = (int)position.z;
-        BlockType blockType = voxelMap[x, y, z];
-
-        if (CheckVoxel(new Vector3(x, y, z - 1))) { AddVoxelFace(position, 0, blockType); }
-        if (CheckVoxel(new Vector3(x, y, z + 1))) { AddVoxelFace(position, 1, blockType); }
-        if (CheckVoxel(new Vector3(x, y + 1, z))) { AddVoxelFace(position, 2, blockType); }
-        if (CheckVoxel(new Vector3(x, y - 1, z))) { AddVoxelFace(position, 3, blockType); }
-        if (CheckVoxel(new Vector3(x - 1, y, z))) { AddVoxelFace(position, 4, blockType); }
-        if (CheckVoxel(new Vector3(x + 1, y, z))) { AddVoxelFace(position, 5, blockType); }
+        return StoneTexture;
     }
 
     void CreateMesh()
@@ -122,7 +113,6 @@ public class Chunk : MonoBehaviour
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.uv = uvs.ToArray();
-
         mesh.RecalculateNormals();
         GetComponent<MeshFilter>().mesh = mesh;
     }
@@ -130,54 +120,52 @@ public class Chunk : MonoBehaviour
     void PopulateVoxelMap()
     {
         for (int x = 0; x < chunkSize; x++)
-        {
             for (int z = 0; z < chunkSize; z++)
             {
                 float worldX = transform.position.x + x;
                 float worldZ = transform.position.z + z;
                 int worldHeight = GetTerrainHeight(worldX, worldZ);
-
                 for (int y = 0; y < chunkHeight; y++)
                 {
-                    if (y == worldHeight - 1) { voxelMap[x, y, z] = BlockType.Grass; }
-                    else if (y < worldHeight - 1 && y > worldHeight - 5) { voxelMap[x, y, z] = BlockType.Dirt; }
-                    else if (y <= worldHeight - 5) { voxelMap[x, y, z] = BlockType.Stone; }
-                    else { voxelMap[x, y, z] = BlockType.Air; }
+                    if (y == worldHeight - 1) voxelMap[x, y, z] = BlockType.Grass;
+                    else if (y < worldHeight - 1 && y > worldHeight - 5) voxelMap[x, y, z] = BlockType.Dirt;
+                    else if (y <= worldHeight - 5) voxelMap[x, y, z] = BlockType.Stone;
+                    else voxelMap[x, y, z] = BlockType.Air;
                 }
             }
-        }
-    }
-
-    void GenerateChunkMesh()
-    {
-        for (int x = 0; x < chunkSize; x++)
-            for (int y = 0; y < chunkHeight; y++)
-                for (int z = 0; z < chunkSize; z++)
-                    if (voxelMap[x, y, z] != BlockType.Air)
-                        CreateVoxel(new Vector3(x, y, z));
-    }
-
-    bool CheckVoxel(Vector3 pos)
-    {
-        int x = Mathf.FloorToInt(pos.x);
-        int y = Mathf.FloorToInt(pos.y);
-        int z = Mathf.FloorToInt(pos.z);
-
-        if (x < 0 || x >= chunkSize || y < 0 || y >= chunkHeight || z < 0 || z >= chunkSize)
-        {
-            Vector3 worldPos = new Vector3(x, y, z) + transform.position;
-            return worldPos.y >= GetTerrainHeight(worldPos.x, worldPos.z);
-        }
-        return voxelMap[x, y, z] == BlockType.Air;
     }
 
     public static int GetTerrainHeight(float worldX, float worldZ)
     {
         float noiseScale = 25f;
-        int terrainMaxHeight = 20;
+        int terrainMaxHeight = 40;
         float noiseX = worldX / noiseScale;
         float noiseZ = worldZ / noiseScale;
         float perlinValue = Mathf.PerlinNoise(noiseX, noiseZ);
         return Mathf.RoundToInt(perlinValue * terrainMaxHeight);
     }
+
+    private static readonly Vector3[] FaceNormals = new Vector3[6]
+    {
+        Vector3.back, Vector3.forward, Vector3.up, Vector3.down, Vector3.left, Vector3.right
+    };
+
+    // --- ¡LA CORRECCIÓN ESTÁ AQUÍ! ---
+    // Esta tabla de datos de vértices por cara está ahora en el orden correcto
+    // para que todas las caras apunten hacia afuera.
+    private static readonly Vector3[,] VoxelFaceData = new Vector3[6, 4]
+    {
+        // Cara Trasera (Z-)
+        {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0)},
+        // Cara Frontal (Z+)
+        {new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1), new Vector3(0, 0, 1)},
+        // Cara Superior (Y+)
+        {new Vector3(0, 1, 0), new Vector3(0, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 0)},
+        // Cara Inferior (Y-)
+        {new Vector3(1, 0, 0), new Vector3(1, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 0)},
+        // Cara Izquierda (X-)
+        {new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(0, 1, 0), new Vector3(0, 0, 0)},
+        // Cara Derecha (X+)
+        {new Vector3(1, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1)}
+    };
 }
--- Assets/_Voxya/Code/Scripts/WorldGenerator.cs
+++ Assets/_Voxya/Code/Scripts/WorldGenerator.cs
@@ -1,47 +1,84 @@
-using System.Collections; // ¡Importante! Necesitamos este namespace para las Corrutinas
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    public static WorldGenerator instance;
-
+    [Header("Chunk Prefab")]
+    public GameObject chunkPrefab;
     [Header("World Settings")]
     public int worldSizeInChunks = 10;
 
-    [Header("Chunk Prefab")]
-    public GameObject chunkPrefab;
-
+    // 1. Diccionario para almacenar y acceder a todos los chunks generados
     public Dictionary<Vector3, Chunk> chunks = new Dictionary<Vector3, Chunk>();
-
-    private void Awake()
-    {
-        if (instance == null) { instance = this; }
-        else { Destroy(gameObject); }
-    }
 
     void Start()
     {
-        // En lugar de llamar directamente a la función, iniciamos la corrutina.
-        StartCoroutine(GenerateWorldCoroutine());
+        StartCoroutine(GenerateWorld());
     }
 
-    // El tipo de retorno ahora es IEnumerator, que es lo que usan las corrutinas.
-    IEnumerator GenerateWorldCoroutine()
+    IEnumerator GenerateWorld()
     {
         for (int x = 0; x < worldSizeInChunks; x++)
         {
             for (int z = 0; z < worldSizeInChunks; z++)
             {
-                Vector3 chunkPosition = new Vector3(x * Chunk.chunkSize, 0, z * Chunk.chunkSize);
-                Chunk newChunk = Instantiate(chunkPrefab, chunkPosition, Quaternion.identity, this.transform).GetComponent<Chunk>();
-                chunks.Add(chunkPosition, newChunk);
-
-                // --- ¡LA LÍNEA CLAVE! ---
-                // Pausa la ejecución aquí y le devuelve el control a Unity.
-                // Reanudará en el siguiente fotograma desde este mismo punto.
-                yield return null;
+                Vector3 position = new Vector3(x * Chunk.chunkSize, 0, z * Chunk.chunkSize);
+                GenerateChunk(position);
+                yield return null; // Pausa de un frame para no congelar Unity
             }
+        }
+    }
+
+    // 2. Función para generar un único chunk, guardarlo y prepararlo
+    void GenerateChunk(Vector3 position)
+    {
+        GameObject chunkObject = Instantiate(chunkPrefab, position, Quaternion.identity);
+        chunkObject.transform.SetParent(transform);
+        Chunk newChunk = chunkObject.GetComponent<Chunk>();
+
+        // Guardamos el chunk en el diccionario y lo inicializamos.
+        // Al inicializar, le pasamos una referencia a este mismo script (el "mundo")
+        // para que el chunk pueda comunicarse con él.
+        chunks.Add(position, newChunk);
+        newChunk.Initialize(this);
+    }
+
+    // 3. Función PÚBLICA para que cualquier chunk pueda preguntar por un bloque
+    public BlockType GetBlockAt(Vector3 worldPosition)
+    {
+        // Redondeamos la posición para asegurarnos de que estamos en la rejilla de bloques
+        int blockX = Mathf.FloorToInt(worldPosition.x);
+        int blockY = Mathf.FloorToInt(worldPosition.y);
+        int blockZ = Mathf.FloorToInt(worldPosition.z);
+
+        // Calculamos a qué chunk pertenece esa coordenada
+        int chunkX = Mathf.FloorToInt(blockX / (float)Chunk.chunkSize) * Chunk.chunkSize;
+        int chunkZ = Mathf.FloorToInt(blockZ / (float)Chunk.chunkSize) * Chunk.chunkSize;
+
+        Vector3 chunkPos = new Vector3(chunkX, 0, chunkZ);
+
+        // Si el chunk que buscamos ya ha sido generado, le preguntamos a él.
+        // Esta es la parte que evita las paredes entre chunks.
+        if (chunks.ContainsKey(chunkPos))
+        {
+            // La lógica para obtener el bloque exacto del otro chunk iría aquí.
+            // Por ahora, una simulación basada en la altura del terreno es suficiente
+            // para que el sistema de visibilidad funcione.
+            int terrainHeight = Chunk.GetTerrainHeight(worldPosition.x, worldPosition.z);
+
+            if (blockY >= terrainHeight) return BlockType.Air;
+            if (blockY == terrainHeight - 1) return BlockType.Grass;
+            if (blockY < terrainHeight - 4) return BlockType.Stone;
+            return BlockType.Dirt;
+        }
+        else
+        {
+            // Si el chunk aún no existe (está fuera del mundo generado),
+            // lo tratamos como si fuera sólido por debajo de la altura del terreno.
+            int terrainHeight = Chunk.GetTerrainHeight(worldPosition.x, worldPosition.z);
+            if (worldPosition.y >= terrainHeight) return BlockType.Air;
+            return BlockType.Stone;
         }
     }
 }
